333e3a5b951fc729de3d180dc1b1026ab2a163d2
crystalcuber <50253028+crystalcuber@users.noreply.github.com>
2025-09-08 00:31:41 -0400

    Try even stricter middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,13 @@
 export { middleware } from "nextra/locales";
 
+// We use this for Nextra's i18n https://nextra.site/docs/guide/i18n
 export const config = {
-  matcher: ["/", "/en/:path*", "/fr/:path*", "/he/:path*", "/zh/:path*"],
+  // Matcher ignoring `/api`, `/_next` and `/assets`
+  // matcher: [
+  //   '/((?!api|_next/static|_next/image|assets|favicon.ico|favicon.png|icon.svg|apple-icon.png|manifest|robots.txt).*)'
+  // ]
+  // TODO: for whatever reason, since Aug 15th 2025, the i18n middleware is using up a ton of Vercel's Fluid Active CPU
+  // Might be because I upgraded the Node.js version from 18 to 22 on Vercel at around that time
+  // Anyway let's redirect zzmethod.com to zzmethod.com/en or whatever the locale is, but not redirect anything else
+  matcher: ["/"],
 };
