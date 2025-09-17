bde39467308376ccf2f011ca44b1b4730bae249a
crystalcuber <50253028+crystalcuber@users.noreply.github.com>
2025-09-16 22:48:39 -0400

    Fix links (#57)
    * Revert "Try even stricter middleware (#56)"
    
    This reverts commit 6e71a3c6c1a0e73bdbd7ddaaad1afd0726ce2a4a.
    
    * Revert "Try stricter middleware matcher (#55)"
    
    This reverts commit 1308851ea940f5bc34f053269f65b62cff23fa54.
    
    * Revert "Fix unnecessary middleware invocation"
    
    This reverts commit 2225d2f70f8f18ae35f68c147b8aeafd74126221.

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,10 @@
-export { middleware } from "nextra/locales";
+export { middleware } from 'nextra/locales'
 
-// We use this for Nextra's i18n https://nextra.site/docs/guide/i18n
+// By default for i18n, links are redirected from zzmethod.com/some/path to zzmethod.com/en/some/path.
+// This behaviour is sometimes unwanted. If you experience 404s due to this, update the below regex to ignore certain files/paths.
 export const config = {
   // Matcher ignoring `/api`, `/_next` and `/assets`
-  // matcher: [
-  //   '/((?!api|_next/static|_next/image|assets|favicon.ico|favicon.png|icon.svg|apple-icon.png|manifest|robots.txt).*)'
-  // ]
-  // TODO: for whatever reason, since Aug 15th 2025, the i18n middleware is using up a ton of Vercel's Fluid Active CPU
-  // Might be because I upgraded the Node.js version from 18 to 22 on Vercel at around that time
-  // Anyway let's redirect zzmethod.com to zzmethod.com/en or whatever the locale is, but not redirect anything else
-  matcher: ["/"],
-};
+  matcher: [
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|icon.svg|apple-icon.png|manifest|robots.txt).*)'
+  ]
+}
