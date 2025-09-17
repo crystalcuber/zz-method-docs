2f4fd8e362e854087ea0846b4ca07746e687f79e
crystalcuber <50253028+crystalcuber@users.noreply.github.com>
2025-09-16 22:44:05 -0400

    Revert "Try stricter middleware matcher (#55)"
    This reverts commit 1308851ea940f5bc34f053269f65b62cff23fa54.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,10 @@
-export { middleware } from "nextra/locales";
+export { middleware } from 'nextra/locales'
 
+// By default for i18n, links are redirected from zzmethod.com/some/path to zzmethod.com/en/some/path.
+// This behaviour is sometimes unwanted. If you experience 404s due to this, update the below regex to ignore certain files/paths.
 export const config = {
-  matcher: ["/", "/en/:path*", "/fr/:path*", "/he/:path*", "/zh/:path*"],
-};
+  // Matcher ignoring `/api`, `/_next` and `/assets`
+  matcher: [
+    '/((?!api|_next|assets|favicon.ico|favico.png|icon.svg|apple-icon.png|manifest|robots.txt).*)'
+  ]
+}
